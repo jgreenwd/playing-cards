46d7013dd834bb9cb193cc2a04f1d9f00e2d5b37
Jeremy Greenwood <jgreenwd@hotmail.com>
2020-03-11 21:24:03 -0500

    remove duplicate files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Deck from './Deck';
+import Hand from './Hand';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            deck: new Deck(),
+            ready: false
+        };
+    }
+
+    componentDidMount() {
+        this.state.deck.shuffle();
+        this.setState({ ready: true });
+    }
+
+    render() {
+        if (this.state.ready) {
+            const cards = [
+                this.state.deck.deal(),
+                this.state.deck.deal(),
+                this.state.deck.deal(),
+                this.state.deck.deal(),
+                this.state.deck.deal()
+            ]
+            return (
+                <div>
+                    <Hand cards={cards} />
+                </div>
+            )
+        } else {
+            return <div></div>
+        }
+    }
 }
 
 export default App;
